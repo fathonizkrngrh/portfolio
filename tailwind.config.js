4ac619f49531c09569f84d0349bea85745e5d78f
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2023-01-19 17:26:35 +0700

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#1363DF",
+        dark: "#06283d",
+        secondary: "#1C82AD",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
